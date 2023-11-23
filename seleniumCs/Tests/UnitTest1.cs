68dbe8f658455b34c871012320c4d8234341bfbf
mnoby <muhammad.noby@gmail.com>
2023-11-23 09:32:02 +0700

    Adjusting Directory Structure and Adding Coverage

--- seleniumCs/Tests/UnitTest1.cs
+++ seleniumCs/Tests/UnitTest1.cs
@@ -4,18 +4,25 @@ using OpenQA.Selenium.Support.UI;
 using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
 using System.Drawing.Printing;
 using System;
+using Utils.Initiates;
+using AventStack.ExtentReports;
+using NUnit.Framework.Interfaces;
+using System.IO;
+using System.Diagnostics;
+using OpenQA.Selenium.Interactions;
 
 namespace seleniumCs
 {
-    public class Tests:Initiate
+    [TestFixture]
+    public class UnitTest : Initiates
     {
-        //[SetUp]   
-       private IWebDriver driver;
-       private URL url;
+        private ChromeDriver _driver;
+        private WebDriverWait _wait;
+        private Actions _actions;
+        private string _url;
 
-        [OneTimeSetUp]
+        [SetUp]
         public void Setup()
-
         {
 
             //Below code is to get the drivers folder path dynamically.
@@ -25,9 +32,15 @@ namespace seleniumCs
             // string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
 
             //Creates the ChomeDriver object, Executes tests on Google Chrome
+            _test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            _driver = new ChromeDriver();
+            _wait = new(_driver, TimeSpan.FromSeconds(10));
+            _actions = new(_driver);
+            _url = "https://practicetestautomation.com/";
 
-            driver = new ChromeDriver();
-            url = new URL(); 
+
+
+            //url = new URL(); 
             // rivate IWebElement practiceBtn => driver.FindElement(By.XPath("//a[contains(text(),'Practice')]"));
 
 
@@ -39,35 +52,98 @@ namespace seleniumCs
 
         }
 
-        [OneTimeTearDown]
+        //[OneTimeTearDown]
+        [TearDown]
         public void TearDown()
         {
-            driver.Quit();
+            _driver.Quit();
         }
 
         [Test]
         public void AccessHomePage()
-
         {
-            //driver.Navigate().GoToUrl("");
-            //driver.Url = url.AutomationTesting;
-            driver.Navigate().GoToUrl("https://practicetestautomation.com/");
-            driver.Manage().Window.Maximize();
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
-            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("menu-primary-items")));
-            Assert.That(driver.PageSource, Does.Contain("Hello"));
-            Console.WriteLine("Success -- Access to Home Page");
+            //_driver.Navigate().GoToUrl("");
+            //_driver.Url = url.AutomationTesting;
+            _driver.Navigate().GoToUrl(_url);
+            _driver.Manage().Window.Maximize();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            var title = _driver.Title;
+
+            try
+            {
+                By menus = By.Id("menu-primary-items");
+                _wait.Until(ExpectedConditions.ElementIsVisible(menus));
+                Assert.That(_driver.PageSource, Does.Contain("Hello"));
+                Console.WriteLine("Success -- Access to Home Page");
+
+            }
+            catch (Exception ex)
+            {
+                throw new TimeoutException("IT TAKES TOO LONG BROO !", ex);
+            }
         }
 
         [Test]
         public void LogInPractice()
         {
-            //AccessHomePage();
-            practiceBtn.Click();
+            AccessHomePage();
+            _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[contains(text(),'Practice')]"))).Click();
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("post-title")));
+            _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[contains(text(),'Test Login Page')]"))).Click();
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.Id("login")));
+            Assert.AreEqual(_driver.FindElement(By.XPath("//*[@id=\"login\"]/h2")).Text, "Test login");
 
+            _driver.FindElement(By.Name("username")).SendKeys("student");
+            _driver.FindElement(By.Name("password")).SendKeys("Password123");
+            _driver.FindElement(By.Id("submit")).Click();
+
+            string currentURL = _driver.Url;
+            IWebElement logout = _driver.FindElement(By.XPath("//a[contains(text(),'Log out')]"));
+            Assert.Multiple(() =>
+            {
+                Assert.That(currentURL, Does.Contain("https://practicetestautomation.com/logged-in-successfully/"));
+                Assert.That(_driver.PageSource, Does.Contain("Congratulations").Or.Contain("successfully logged in"), "The Page is Containing Congratulations Or Successfully Logged in");
+                Assert.That(logout.Displayed,"Log Out Button Has Been Displayed");
+            });
 
         }
 
+        [Test]
+        public void KeyUpTest()
+        {
+            AccessHomePage();
+            _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[contains(text(),'Practice')]"))).Click();
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("post-title")));
+            _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[contains(text(),'Test Login Page')]"))).Click();
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.Id("login")));
+            Assert.AreEqual(_driver.FindElement(By.XPath("//*[@id=\"login\"]/h2")).Text, "Test login");
 
+            IWebElement username = _driver.FindElement(By.Name("username"));
+            username.Click();
+            _actions
+                 .KeyDown(Keys.Shift)
+                 .SendKeys("s")
+                 .KeyUp(Keys.Shift)
+                 .SendKeys("tudent")
+                 .Perform();
+
+            Console.WriteLine($">>>>>>>> Username Value: {username.GetAttribute("value")}");
+            Assert.AreEqual(username.GetAttribute("value"), "Student", "First Letter is UpperCase!");
+        }
+
+
+        [Test]
+        public void ScrollToElement()
+        {
+            AccessHomePage();
+            IWebElement subscribeButton = _driver.FindElement(By.XPath("//*[@data-automation-id=\"subscribe-submit-button\"]"));
+            var firstSubscribeColor = subscribeButton.GetCssValue("background-color");
+            _actions.MoveToElement(subscribeButton);
+            _actions.Perform();
+
+            var secondSubscribeColor = subscribeButton.GetCssValue("background-color");
+            Assert.AreNotEqual(secondSubscribeColor, firstSubscribeColor, "The Button Color Has Been Changed due to mouse hover!");
+            Thread.Sleep(5000);
+        }
     }
 }
---
+++ seleniumCs/Utils/Initiates.cs
@@ -0,0 +1,112 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using seleniumCs.Utils;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.IO;
+using System.Reflection;
+//using OpenQA.Selenium;
+using NUnit.Framework.Interfaces;
+
+namespace Utils.Initiates;
+
+[SetUpFixture]
+public abstract class Initiates
+{
+	//protected IWebDriver driver;
+	//protected WebDriverWait wait;
+	public static ExtentReports _extent;
+	public static ExtentTest _test;
+    private readonly Guid uuid = Guid.NewGuid();
+
+
+	[OneTimeSetUp]
+    protected void SetUp()
+    {
+        //BrowserInitialize();
+        StartReporter();
+    }
+
+    private void StartReporter()
+	{
+        string uid = uuid.ToString().Substring(0,4);
+        var dir = TestContext.CurrentContext.TestDirectory;
+        string actualPath = dir.Substring(0, dir.LastIndexOf("bin")) + @"Reports\";
+        var fileName = this.GetType().ToString() + $"-{uid}" + ".html";
+        var htmlReporter = new ExtentSparkReporter(actualPath + fileName);
+        
+
+        _extent = new ExtentReports();
+        _extent.AttachReporter(htmlReporter);
+    }
+    //private void BrowserInitialize() 
+    //{
+    //	ChromeOptions option = new();
+    //	option.AddArgument("--headless");
+    //	option.AddArgument("--disable-gpu");
+    //	driver = new ChromeDriver(option);
+    //	driver.Manage().Window.Maximize();	
+    //	driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+    //}
+
+    //public void NavigateToUrl(string url)
+    //{
+    //	driver.Navigate().GoToUrl(url);	
+    //}
+    [TearDown]
+    public void AfterTest()
+    {
+        var status = TestContext.CurrentContext.Result.Outcome.Status;
+        var stacktrace = string.IsNullOrEmpty(TestContext.CurrentContext.Result.StackTrace)
+        ? ""
+        : string.Format("{0}", TestContext.CurrentContext.Result.StackTrace);
+        Status logstatus;
+
+        switch (status)
+        {
+            case TestStatus.Failed:
+                logstatus = Status.Fail;
+                break;
+            case TestStatus.Inconclusive:
+                logstatus = Status.Warning;
+                break;
+            case TestStatus.Skipped:
+                logstatus = Status.Skip;
+                break;
+            default:
+                logstatus = Status.Pass;
+                break;
+        }
+
+        _test.Log(logstatus, "Test ended with " + logstatus + stacktrace);
+        _extent.Flush();
+    }
+
+    [OneTimeTearDown]
+    protected void TearDown()
+    {
+        _extent.Flush();
+    }
+ //   public void Close()
+	//{
+	//	
+	//}
+
+
+
+ //   public void ExplicitWaitVisible(object element)
+	//{
+	//	wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+	//	var waitingEl = wait.Until(ExpectedConditions.ElementIsVisible((By)element));
+ //       Assert.IsTrue(waitingEl.Displayed);
+	//}
+
+ //   public void ExplicitWaitClickable(object element)
+ //   {
+ //       wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+ //       wait.Until(ExpectedConditions.ElementToBeClickable((By)element)).Click();
+ //   }
+}
